5c5f047a0b02552400ab960cd0e5acae9d9741c2
David Iraheta <david.iraheta.89@gmail.com>
2024-12-26 12:03:53 -0500

    Python201.12.26

--- 02_more-datatypes/02_18_advanced_sorting.py
+++ 02_more-datatypes/02_18_advanced_sorting.py
@@ -10,3 +10,6 @@
 # Feel free to discuss any questions you have with your mentor and on the forum!
 new_dict = {"item1": 5, "item2": 6, "item3": 1}
 tuples_list = [(key, value) for key, value in new_dict.items()]
+sorted_list = sorted(tuples_list, key=lambda x: x[1])
+print(sorted_list)
+
